4bd241a906588e60a0daf77859f251d85b07aa6a
Areeha Adil <adilareeha@gmail.com>
2026-05-10 13:47:30 +0500

    Update settings to allow GMAIL_CREDENTIALS_JSON from environment

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,6 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# If running on Streamlit Cloud, we can pass the JSON content directly as an environment variable
+# and write it to the file path expected by the rest of the application.
+if "GMAIL_CREDENTIALS_JSON" in os.environ:
+    with open("./GMAIL_CREDENTIALS.json", "w") as f:
+        f.write(os.environ["GMAIL_CREDENTIALS_JSON"])
+
 required_keys = [
     "OPENAI_API_KEY",
     "NOTION_API_KEY",
